48062c5306947c9890d7f1ce6c6a5a4a72cdf3b2
praveengamini <praveengamini009@gmail.com>
2025-11-22 03:47:24 +0530

    path export

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "Installing dependencies..."
+echo ">>> Setting PYTHONPATH"
+export PYTHONPATH=/opt/render/project/src
+
+echo ">>> Installing dependencies..."
 pip install -r requirements.txt
 
-echo "Downloading dataset..."
+echo ">>> Downloading dataset..."
 python3 src/preprocess/download_data.py
 
-echo "Building embeddings + FAISS index..."
+echo ">>> Building embeddings + FAISS index..."
 python3 src/main.py
 
-echo "Build complete!"
+echo ">>> Build complete!"
